020b29fb83f567275ded32e9eb85dbc5ab0157ef
namratha <namrathanm2612@gmail.com>
2021-06-23 12:54:07 +0530

    coin placemnet value change

--- src/app/admin/admin-settings/admin-settings.component.ts
+++ src/app/admin/admin-settings/admin-settings.component.ts
@@ -124,6 +124,75 @@ export class AdminSettingsComponent implements OnInit {
     });
   }
 
+    refreshCoin() {
+    var data = {
+      userId: this.loginData.userData
+    }
+    this.api.getSuperAdminCoin(data).then((res: any) => {
+      console.log("coin submit====", res);
+      this.coinData = [];
+      if (res.status) {
+        this.coinData = res.success;
+      }
+      else {
+        this.coinData = [];
+      }
+    }).catch((err: any) => {
+      console.log("error===", err);
+    })
+  }
+
+  refreshDevice() {
+    var data = {
+      userId: this.loginData.userData
+    }
+    this.api.viewSuperAdminDevice(data).then((res: any) => {
+      this.deviceData = [];
+      console.log("find submit====", res);
+      if (res.status) {
+        this.deviceData = res.success;
+      }
+      else {
+        this.deviceData = [];
+      }
+
+    }).catch((err: any) => {
+      console.log("error===", err);;
+    })
+  }
+
+  refreshGateway() {
+    var data = {
+      userId: this.loginData.userData
+    }
+    console.log("user id data==", data);
+    this.api.getSuperAdminGatewayData(data).then((res: any) => {
+      console.log("gatway submit====", res);
+      this.gateway = [];
+      if (res.status) {
+        this.gateway = res.success;
+      }
+    }).catch((err: any) => {
+      console.log("error===", err);
+    })
+  }
+
+  getGroups() {
+    var data = {
+      userId: this.loginData.userData
+    }
+    this.api.getGroup(data).then((res: any) => {
+      console.log("group details response==", res);
+      this.groupData = [];
+      if (res.status) {
+        this.groupData = res.success;
+      }
+      else {
+        this.groupData = [];
+      }
+    })
+  }
+
   refreshSettings(data) {
     this.api.getUserSettings(data).then((res: any) => {
       console.log("user settings ===", res)
@@ -155,143 +224,141 @@ export class AdminSettingsComponent implements OnInit {
         this.deletionTimeForm.patchValue({
           deletionTime: res.success.deletionTime
         });
+        
         this.inOutmergingTimeForm.patchValue({
           inOutMergeTime: res.success.inOutMergeTime
         });
+
         this.rangeForm.patchValue({
           range: res.success.range
         });
-
-
-
       }
     })
   }
+
   onSubmitOnlineStatus(data) {
     if (this.onlineStatus.valid) {
-      data.userId = this.loginData.userData
-      console.log("onSubmit OnlineStatus data==", data)
+      data.userId = this.loginData.userData;
+      console.log("onSubmit OnlineStatus data==", data);
       this.api.updateOnlineStatus(data).then((res: any) => {
-        console.log("res==", res)
+        console.log("res==", res);
         if (res.status) {
-          this.general.openSnackBar(res.message, '')
-          this.refreshSettings(data.userId)
+          this.general.openSnackBar(res.message, '');
+          this.refreshSettings(data.userId);
         }
       }).catch((err: any) => {
-        console.log("err==", err)
+        console.log("err==", err);
       })
     }
   }
 
   onSubmitOfflineStatus(data) {
     if (this.offlineStatus.valid) {
-      data.userId = this.loginData.userData
-      console.log("onSubmit OfflineStatus data==", data)
+      data.userId = this.loginData.userData;
+      console.log("onSubmit OfflineStatus data==", data);
       this.api.updateOfflineStatus(data).then((res: any) => {
-        console.log("res==", res)
+        console.log("res==", res);
         if (res.status) {
-          this.general.openSnackBar(res.message, '')
-          this.refreshSettings(data.userId)
+          this.general.openSnackBar(res.message, '');
+          this.refreshSettings(data.userId);
         }
       }).catch((err: any) => {
-        console.log("err==", err)
+        console.log("err==", err);
       })
     }
   }
 
   onSubmitTxPower(data) {
     if (this.txPowerForm.valid) {
-      data.userId = this.loginData.userData
+      data.userId = this.loginData.userData;
       console.log("onSubmit tx power data==", data)
       this.api.updateTxPower(data).then((res: any) => {
-        console.log("res==", res)
+        console.log("res==", res);
         if (res.status) {
-          this.general.openSnackBar("TX power updated successfully!!!", '')
+          this.general.openSnackBar("TX power updated successfully!!!", '');
           this.refreshSettings(data.userId);
         }
       }).catch((err: any) => {
-        console.log("err==", err)
+        console.log("err==", err);
       })
     }
   }
 
   onSubmitRssi(data) {
     if (this.rssiForm.valid) {
-      data.userId = this.loginData.userData
-      console.log("onSubmit rssi data==", data)
+      data.userId = this.loginData.userData;
+      console.log("onSubmit rssi data==", data);
       this.api.updateRssi(data).then((res: any) => {
         if (res.status) {
-          console.log("res==", res)
+          console.log("res==", res);
           this.general.openSnackBar(res.message, '');
           this.refreshSettings(data.userId);
         }
       }).catch((err: any) => {
-        console.log("err==", err)
+        console.log("err==", err);
       })
     }
   }
-  onSubmitShiftForm(data) {
-    var cdt1 = moment(data.startTime, 'HH:mm:ss')
-    var cdt2 = moment(data.endTime, 'HH:mm:ss')
-    var times1 = moment(cdt1).format("YYYY/MM/DD HH:mm:ss")
-    var times2 = moment(cdt2).format("YYYY/MM/DD HH:mm:ss")
-    if (times1 > times2 || (data.fromTime == "00:00" && data.endTime == "00:00")) {
-      times2 = moment(cdt2).add(1, 'days').format("YYYY/MM/DD HH:mm:ss")
-    }
-    var times = moment(times2, "YYYY/MM/DD HH:mm:ss").diff(moment(times1, "YYYY/MM/DD HH:mm:ss"))
-    var d = moment.duration(times)
 
-    var minhour = (d.hours() + ":" + d.minutes()).split(":")
+  onSubmitShiftForm(data) {
+    var cdt1 = moment(data.startTime, 'HH:mm:ss');
+    var cdt2 = moment(data.endTime, 'HH:mm:ss');
+    var times1 = moment(cdt1).format("YYYY/MM/DD HH:mm:ss");
+    var times2 = moment(cdt2).format("YYYY/MM/DD HH:mm:ss");
+    if (times1 > times2 || (data.fromTime == "00:00" && data.endTime == "00:00")) {
+      times2 = moment(cdt2).add(1, 'days').format("YYYY/MM/DD HH:mm:ss");
+    }
+    var times = moment(times2, "YYYY/MM/DD HH:mm:ss").diff(moment(times1, "YYYY/MM/DD HH:mm:ss"));
+    var d = moment.duration(times);
+
+    var minhour = (d.hours() + ":" + d.minutes()).split(":");
 
     if ((parseInt(minhour[0]) >= 9 && (parseInt(minhour[1]) >= 0 && parseInt(minhour[1]) <= 59))) {
-      this.timeExceed = false
-      var dateobj = new Date()
+      this.timeExceed = false;
+      var dateobj = new Date();
 
       var year = dateobj.getFullYear();
-      var month = dateobj.getMonth() + 1
-      var day = dateobj.getDate()
-      var date = month + '/' + day + '/' + year
+      var month = dateobj.getMonth() + 1;
+      var day = dateobj.getDate();
+      var date = month + '/' + day + '/' + year;
 
-      var time1 = date + " " + data.startTime
-      var time2 = date + " " + data.endTime
+      var time1 = date + " " + data.startTime;
+      var time2 = date + " " + data.endTime;
 
-      time1 = new Date(time1).toUTCString()
-      time2 = new Date(time2).toUTCString()
-      var h = new Date(time1).getUTCHours()
-      var m = new Date(time1).getUTCMinutes()
-      var h1 = new Date(time2).getUTCHours()
-      var m1 = new Date(time2).getUTCMinutes()
+      time1 = new Date(time1).toUTCString();
+      time2 = new Date(time2).toUTCString();
+      var h = new Date(time1).getUTCHours();
+      var m = new Date(time1).getUTCMinutes();
+      var h1 = new Date(time2).getUTCHours();
+      var m1 = new Date(time2).getUTCMinutes();
       var hh = h <= 9 && h >= 0 ? "0" + h : h;
       var mm = m <= 9 && m >= 0 ? "0" + m : m;
       var hh1 = h1 <= 9 && h1 >= 0 ? "0" + h1 : h1;
       var mm1 = m1 <= 9 && m1 >= 0 ? "0" + m1 : m1;
 
-      data.startTime = hh + ':' + mm
-      data.endTime = hh1 + ':' + mm1
-
-
+      data.startTime = hh + ':' + mm;
+      data.endTime = hh1 + ':' + mm1;
 
       if (this.shiftForm.valid) {
         try {
           //  console.log("time data===",data)
-          data.userId = this.loginData.userData
-          console.log("data====", data)
+          data.userId = this.loginData.userData;
+          console.log("data====", data);
           this.api.createdDeviceShift(data).then((res: any) => {
             //  console.log("time insrted or updated",res)
             if (res.status) {
-              this.timeExceed = false
-              this.multipleShift = false
-
-              this.general.openSnackBar('Shift updated successfully!!', '')
+              this.timeExceed = false;
+              this.multipleShift = false;
+              this.general.openSnackBar('Shift updated successfully!!', '');
               this.shiftForm.reset();
             }
             else {
-              this.timeExceed = false
-              this.multipleShift = true
+              this.timeExceed = false;
+              this.multipleShift = true;
             }
           })
-
         } catch (err) {
+          console.log("error===",err)
         }
       }
     }
@@ -610,82 +677,6 @@ export class AdminSettingsComponent implements OnInit {
         this.refreshGateway();
       }
     });
-  }
-
-  refreshCoin() {
-    var data = {
-      userId: this.loginData.userData
-    }
-    this.api.getSuperAdminCoin(data).then((res: any) => {
-      console.log("coin submit====", res);
-      this.coinData = [];
-      if (res.status) {
-        this.coinData = res.success;
-      }
-      else {
-        this.coinData = [];
-      }
-
-    }).catch((err: any) => {
-      console.log("error===", err);
-    })
-  }
-
-  refreshDevice() {
-    var data = {
-      userId: this.loginData.userData
-    }
-    this.api.viewSuperAdminDevice(data).then((res: any) => {
-
-      this.deviceData = [];
-      console.log("find submit====", res);
-      if (res.status) {
-        this.deviceData = res.success;
-      }
-      else {
-        this.deviceData = [];
-      }
-
-    }).catch((err: any) => {
-      console.log("error===", err);;
-    })
-  }
-
-  refreshGateway() {
-    var data = {
-      userId: this.loginData.userData
-    }
-    console.log("user id data==", data);
-
-    this.api.getSuperAdminGatewayData(data).then((res: any) => {
-
-      console.log("gatway submit====", res);
-      this.gateway = []
-      if (res.status) {
-        this.gateway = res.success
-      }
-
-    }).catch((err: any) => {
-      console.log("error===", err)
-    })
-  }
-
-  getGroups() {
-    var data = {
-      userId: this.loginData.userData
-    }
-
-    this.api.getGroup(data).then((res: any) => {
-
-      console.log("group details response==", res);
-      this.groupData = [];
-      if (res.status) {
-        this.groupData = res.success;
-      }
-      else {
-        this.groupData = [];
-      }
-    })
   }
 
   toggleAllSelectionDevice(formData) {
